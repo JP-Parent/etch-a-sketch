4203134edc3e90f9ff5b1a983b191df26fd6f8e6
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-26 19:50:27 -0400

    refac pour assurer affichage est loaded

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
-var container = document.createElement("div");
-container.setAttribute("class", "container");
-document.body.appendChild(container);
+document.addEventListener('DOMContentLoaded', function() {
+    var container = document.createElement("div");
+    container.setAttribute("class", "container");
+    document.body.appendChild(container);
 
-
-for (var i = 0; i < 9; i++) {
-    var row = document.createElement("div");
-    row.setAttribute("class", "row");
-    container.appendChild(row);
-
-    for (var j = 0; j < 9; j++) {
+    for (var i = 0; i < 100 * 100; i++) {
         var square = document.createElement("div");
-        square.setAttribute("class", "grid");
-        row.appendChild(square);
+        square.setAttribute("class", "square");
+        square.style.width = "calc(100%/100)";
+        square.style.height = "calc(100%/100)";
+        container.appendChild(square);
     }
-}
+});
