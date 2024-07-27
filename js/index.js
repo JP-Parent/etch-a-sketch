bda212a0f08fa95fde2af2f4234705ee1099e64b
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-27 16:26:34 -0400

    added button for grid dimension and color hover on squares

--- js/index.js
+++ js/index.js
@@ -1,13 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.createElement("button");
+    button.setAttribute("class", "grid-set");
+    button.innerText = "Click me!"
+    document.body.appendChild(button)
+
     var container = document.createElement("div");
     container.setAttribute("class", "container");
     document.body.appendChild(container);
 
-    for (var i = 0; i < 100 * 100; i++) {
-        var square = document.createElement("div");
-        square.setAttribute("class", "square");
-        square.style.width = "calc(100%/100)";
-        square.style.height = "calc(100%/100)";
-        container.appendChild(square);
+    function createGrid(squares) {
+        container.innerHTML = "";
+        for (var i = 0; i < squares * squares; i++) {
+            let square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.style.width = `calc(100%/${squares})`;
+            square.style.height = `calc(100%/${squares})`;
+            container.appendChild(square);
+        }
+        colorSquaresHover();
+    }
+
+    createGrid(16);
+
+    button.addEventListener("click", function () {
+        var size = prompt("How many squares per side would you like? (max 100)")
+        if (size && !isNaN(size) && !(size > 100)) {
+            createGrid(size)
+        }
+        else alert("Enter a valid value!")
+    })
+
+    function randomColor() {
+        let color = [];
+        for (let i = 0; i < 3; i++) {
+            color.push(Math.floor(Math.random() * 256))
+        }
+        return "rgb(" + color.join(", ") + ")";
+    }
+
+    function colorSquaresHover() {
+        var squares = document.querySelectorAll(".square");
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener("mouseover", function () {
+                squares[i].style.backgroundColor = randomColor();
+            })
+            squares[i].addEventListener("mouseout", function () {
+                squares[i].style.backgroundColor = "white";
+            })
+        }
     }
 });
